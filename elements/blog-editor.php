351c943badba2e14329180fb7a81a11bb0ac8fcb
MrLikviduk <v@086.kz>
2018-05-27 17:36:05 +0600

    v1.0

--- elements/blog-editor.php
+++ elements/blog-editor.php
@@ -4,7 +4,7 @@
         $header = $_POST['header'];
         $content = $_POST['content'];
         $tags = $_POST['tags'];
-        $mysqli->query("INSERT INTO blog (id, header, content, creation_date, tags) VALUES (NULL, $header, $content, $date, $tags)");
+        $mysqli->query("INSERT INTO blog (id, ".'name'.", content, creation_date, tags) VALUES (NULL, $header, $content, $date, $tags)");
         unset($_POST['header']);
     }
 ?>
