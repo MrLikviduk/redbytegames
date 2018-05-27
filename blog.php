4b0b7f03ef508bfef59c37b1b39ad2de175eebe3
MrLikviduk <v@086.kz>
2018-05-27 17:07:22 +0600

    v1.0

--- blog.php
+++ blog.php
@@ -1,9 +1,9 @@
 <?php
     $page_name = 'Блог';
+    include('elements/connection-info.php');
     $mysqli = new mysqli($host_name, $db_username, $db_password, $db_name);
     include('header.php');
     include('elements/to-authorizate.php');
-    include('elements/connection-info.php');
     include('elements/blog-template.php');
     if ($_SESSION['logged_in'] == TRUE) {
         include('blog-editor.php');
