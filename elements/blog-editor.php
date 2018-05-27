789c3acf10690ef699d55ea44f52b6e23f194d54
MrLikviduk <v@086.kz>
2018-05-27 19:18:27 +0600

    v1.0

--- elements/blog-editor.php
+++ elements/blog-editor.php
@@ -1,6 +1,8 @@
 <?php
     if(isset($_POST['header'])) {
         $date = date('Y-m-d');
+        if (isset($mysqli)) echo "YES";
+        else echo "NO";
         $header = $_POST['header'];
         $content = $_POST['content'];
         $tags = $_POST['tags'];
