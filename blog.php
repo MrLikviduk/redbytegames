a3fbc8ad6bff9f3b2a4534a57e85e1bc8e5d7fc7
MrLikviduk <v@086.kz>
2018-05-28 14:55:22 +0600

    v1.0

--- blog.php
+++ blog.php
@@ -1,14 +1,24 @@
 <?php
     $page_name = 'Блог';
     session_start();
-    include('header.php');
     include('elements/connection-info.php');
     $mysqli = new mysqli($host_name, $db_username, $db_password, $db_name) or die("Error to connect to db");
-    include('elements/to-authorizate.php');
-    include('elements/blog-template.php');
+    include('elements/to-authorizate.php')
     if ($_SESSION['logged_in'] == TRUE) {
         include('elements/blog-editor.php');
-    }
+    };
+    include('elements/blog-template.php');
+    include('header.php');
+?>
+<form action="" method="POST" style="margin-left: 25vw;" id="edit_form">
+    <input type="text" name="header" placeholder="Введите название">
+    <input type="text" name="tags" placeholder="Введите теги" style="width: 25vw">
+    <br>
+    <textarea name="content" style="width: 40vw; height: 200px;">Контент</textarea>
+    <br>
+    <input type="submit" name="submit">
+</form>
+<?php
     $result = $mysqli->query("SELECT * FROM blog ORDER BY id DESC");
     while($row = $result->fetch_assoc()) {
         $date = explode('-' ,$row['creation_date']);
