aba6a3aa8faa0801fa2fb9bd49bfa134cd89656e
MrLikviduk <v@086.kz>
2018-05-28 13:01:57 +0600

    v1.0

--- elements/blog-editor.php
+++ elements/blog-editor.php
@@ -1,5 +1,9 @@
 <?php
-    if(isset($_POST['header'])) {
+    function setRandomSessid() {
+	    $_SESSION['sessid'] = md5(date('d.m.Y H:i:s').rand(1, 1000000));
+    }
+    setRandomSessid();
+    if(isset($_POST['header']) && $_POST['sessid'] == $_SESSION['sessid']) {
         $date = date('Y-m-d');
         $header = $_POST['header'];
         $content = $_POST['content'];
@@ -14,5 +18,6 @@
     <br>
     <textarea name="content" style="width: 40vw; height: 200px;">Контент</textarea>
     <br>
+    <input type='hidden' name='sessid' value='<?=$_SESSION['sessid']?>'>
     <input type="submit" name="submit">
 </form>
