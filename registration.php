758bd98574e13eaee45dd8b8b2e15624f343287a
MrLikviduk <v@086.kz>
2018-06-10 10:24:57 +0600

    test

--- registration.php
+++ registration.php
@@ -33,8 +33,8 @@
         echo '<p class="error-text" style="display: none;" id="'.$type_name.'_error_text">Количество символов не должно быть меньше '.$min.'</p>';
         echo "
             <script>
-                function changed_input (obj) {
-                    var value = $(obj).val();
+                function changed_input (obj_input) {
+                    var value = $(obj_input).val();
                     var error = $('#".$type_name."_error_text');
                     if ('".$name."' == 'confirm_password') {
                         if ($('#".$type.'_'.'password'."').val() != value) {
