99eb7fec930594e47280d6de7e77c5e0020e71d2
MrLikviduk <v@086.kz>
2018-06-11 15:50:41 +0600

    test

--- elements/blog-template.php
+++ elements/blog-template.php
@@ -58,7 +58,7 @@
                 }
                 if (can_do('ban_users')) {
                     echo '
-                        <button class="btn" name="ban_user" value="'.$id.'" onclick="return confirm(\'Вы действительно хотите заблокировать пользователя?\')">Заблокировать</button> на <input type="number" name="days" style="width: 40px"> дней <input type="number" name="hours" style="width: 40px"> часов
+                        <button class="btn" name="ban_user" value="'.$id.'" onclick="return confirm(\'Вы действительно хотите заблокировать пользователя?\')">Заблокировать</button> на <input type="number" name="days" style="width: 50px" value="0" max="10000"> дней <input type="number" name="hours" style="width: 50px" value="23"> часов
                     ';
                 }
                 if (is_own_comment($id) || can_do('delete_comments')) {
