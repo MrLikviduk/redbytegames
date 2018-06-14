9a06411203c40e22fa3fa59be369944a004d8d4f
MrLikviduk <v@086.kz>
2018-06-14 08:47:38 +0600

    test

--- project.php
+++ project.php
@@ -111,7 +111,7 @@
     }
     $result = $mysqli->query("SELECT * FROM projects_comments");
     while ($row = $result->fetch_assoc()) {
-        show_comment($row['name'], $row['creation_date'], $row['creation_time'],  $row['content'], $row['rating'], $row['id']);
+        show_comment(get_username_by_id($row['user_id']), $row['creation_date'], $row['creation_time'],  $row['content'], $row['rating'], $row['id']);
     }
     
 ?>
