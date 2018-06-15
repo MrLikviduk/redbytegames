1e3a7c1efa2f3f494c86cb6a9a6161e906de48c0
MrLikviduk <v@086.kz>
2018-06-15 14:33:56 +0600

    test

--- elements/vacancy-template.php
+++ elements/vacancy-template.php
@@ -14,7 +14,7 @@
                 ';
                 if (can_do('edit_vacancy')) echo '
                 <form action="" method="POST">
-                    <button class="btn" name="delete_vacancy" value="'.$id.'">Удалить</button>
+                    <button class="btn" name="delete_vacancy" value="'.$id.'" onclicked="return confirm(\'Вы действительно хотите удалить вакансию?\')">Удалить</button>
                     <button class="btn" name="edit_vacancy" value="'.$id.'">Редактировать</button>
                 </form>
                 ';
