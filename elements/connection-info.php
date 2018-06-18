ecd167ca4a47f01298d6edfa626e083f3ca53d05
MrLikviduk <v@086.kz>
2018-06-18 15:15:40 +0600

    test

--- elements/connection-info.php
+++ elements/connection-info.php
@@ -1,10 +1,10 @@
 <?php
-    // $host_name = 'srv-pleskdb16.ps.kz:3306';
-    // $db_name = 'redbyteg_users';
-    // $db_username = 'redby_proger';
-    // $db_password = 'imgnida1234';
-    $host_name = 'localhost';
-    $db_name = 'users';
-    $db_username = 'root';
-    $db_password = '';
+    $host_name = 'srv-pleskdb16.ps.kz:3306';
+    $db_name = 'redbyteg_users';
+    $db_username = 'redby_proger';
+    $db_password = 'imgnida1234';
+    // $host_name = 'localhost';
+    // $db_name = 'users';
+    // $db_username = 'root';
+    // $db_password = '';
 ?>
