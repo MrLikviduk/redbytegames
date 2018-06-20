0b69bc1ced66c2e5867f643b05daf878a5d06d93
MrLikviduk <v@086.kz>
2018-06-20 14:31:06 +0600

    test

--- elements/support-result.php
+++ elements/support-result.php
@@ -2,13 +2,12 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php');
     require_once($_SERVER['DOCUMENT_ROOT'].'/elements/constants.php');
     $mysqli = connect_to_database();
-    if (true) {
-        if (is_legal($_POST['fullname'], FULLNAME_MIN, FULLNAME_MAX) && is_legal($_POST['email'], EMAIL_MIN, EMAIL_MAX) && is_legal($_POST['message'], 1, 1023) && check_captcha($_POST['g-recaptcha-response'])) {
-            $fullname = $mysqli->real_escape_string($_POST['fullname']);
-            $email = $mysqli->real_escape_string($_POST['email']);
-            $message = $mysqli->real_escape_string($_POST['message']);
-            $mysqli->query("INSERT INTO support_requests (id, fullname, `email`, `message`) VALUES (NULL, '$fullname', '$email', '$message')") or die("ERROR");
-        }
+    if (is_legal($_POST['fullname'], FULLNAME_MIN, FULLNAME_MAX) && is_legal($_POST['email'], EMAIL_MIN, EMAIL_MAX) && is_legal($_POST['message'], 1, 1023) && check_captcha($_POST['g-recaptcha-response'])) {
+        $fullname = $mysqli->real_escape_string($_POST['fullname']);
+        $email = $mysqli->real_escape_string($_POST['email']);
+        $message = $mysqli->real_escape_string($_POST['message']);
+        mail('support@redbytegames.ru', 'Новый вопрос', 'ФИО: '.$_POST['fullname'].'<br>Почта: '.$_POST['email'].'<br>Сообщение: '.$_POST['message'], "From: support@redbytegames.ru");
+        $mysqli->query("INSERT INTO support_requests (id, fullname, `email`, `message`) VALUES (NULL, '$fullname', '$email', '$message')") or die("ERROR");
     }
     $mysqli->close();
 ?>
