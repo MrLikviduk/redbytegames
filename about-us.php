8dda35e6718086251bc08890060e73eb7fc50939
MrLikviduk <v@086.kz>
2018-07-04 20:31:11 +0600

    v1.0

--- about-us.php
+++ about-us.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
+    require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php');
     $page_name = 'О нас';
     include('header.php');
 ?>
 <section class="some-important-info">
-    <h1>О нас</h1>
+    <h1><?=translate('О нас')?></h1>
     <div class="main-info">
         <p>Мы - новоиспеченная компания специализирующаяся на разработке и переиздании мобильных игр. 
             Наша цель - приносить людям самый драйвовый, уникальный и запоминающийся игровой опыт.</p>
