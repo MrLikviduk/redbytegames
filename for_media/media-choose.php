da8d86698216482e1ea52cc2113cfbc456641e10
MrLikviduk <v@086.kz>
2018-07-06 19:10:37 +0600

    test

--- for_media/media-choose.php
+++ for_media/media-choose.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functios.php');
+    require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php');
     $page_name = 'Для прессы';
     include($_SERVER['DOCUMENT_ROOT'].'/header.php');
 ?>
