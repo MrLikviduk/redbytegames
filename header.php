f668667a2e3526e9ff92cddf1287a02a608139dd
MrLikviduk <v@086.kz>
2018-07-06 17:42:30 +0600

    test

--- header.php
+++ header.php
@@ -90,7 +90,7 @@
             <a href="/projects.php" onclick="menu.classList.toggle('menu_is-open'); el1.classList.toggle('el1-open'); el2.classList.toggle('el2-open'); el3.classList.toggle('el3-open');">
                 <div class="nav-element"><?=translate('Проекты')?></div>
             </a>
-            <?php can_do('see_info_for_media') { ?>
+            <?php if (can_do('see_info_for_media')) { ?>
                 <a href="#" onclick="menu.classList.toggle('menu_is-open'); el1.classList.toggle('el1-open'); el2.classList.toggle('el2-open'); el3.classList.toggle('el3-open');">
                     <div class="nav-element"><?=translate('Для прессы')?></div>
                 </a>
