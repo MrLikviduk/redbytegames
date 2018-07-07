0923bdc30bc23fea48f58e9c0199927dc6a428ab
MrLikviduk <v@086.kz>
2018-07-07 20:49:44 +0600

    test

--- for_media/media-question-answer-result.php
+++ for_media/media-question-answer-result.php
@@ -3,7 +3,7 @@
     require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php');
     $mysqli = connect_to_database();
     if (can_do('see_info_for_media')) {
-        if (isset($_POST['submit']) && is_legal($_POST['question'], 1, 300)) {
+        if (is_legal($_POST['question'], 1, 300)) {
             $question = $mysqli->real_escape_string($_POST['question']);
             $date = date('d.m.Y');
             $mysqli->query("INSERT INTO questions_answers (id, question, creation_date) VALUES (NULL, '$question', '$date')") or die("Ошибка при запросе в базу данных!");
