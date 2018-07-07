d8e7cda6c91e66fd8d4e157bf9c25181bf78b028
MrLikviduk <v@086.kz>
2018-07-07 07:51:53 +0600

    test

---
+++ for_media/media-kits.php
@@ -0,0 +1,13 @@
+<?php
+    session_start();
+    $page_name = 'Киты для прессы';
+    require_once($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php');
+    if (!can_do('see_info_for_media')) {
+        include($_SERVER['DOCUMENT_ROOT'].'/header.php');
+        echo '<div style="margin: 30px auto;">'.translate('У вас нет прав для просмотра данной страницы').'</div>';
+        include($_SERVER['DOCUMENT_ROOT'].'/footer.php');
+        exit();
+    }
+    include($_SERVER['DOCUMENT_ROOT'].'/header.php');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/footer.php'); ?>
