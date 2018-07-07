6f1530941155ff6f50a582db976461420622cd09
MrLikviduk <v@086.kz>
2018-07-07 12:16:04 +0600

    test

--- elements/media-template.php
+++ elements/media-template.php
@@ -1,6 +1,5 @@
 <?php
-    function show_kit($name, $extension, $date, $id) {
-        include($_SERVER['DOCUMENT_ROOT'].'/elements/functions.php') ?>
+    function show_kit($name, $extension, $date, $id) { ?>
         <div class="kit">
             <div class="date"><?=htmlspecialchars($date, ENT_QUOTES, 'UTF-8')?></div>
             <img src="/img/extension_img/<?=htmlspecialchars($extension, ENT_QUOTES, 'UTF-8')?>.png" alt="<?=htmlspecialchars($extension, ENT_QUOTES, 'UTF-8')?>">
