e0bd323b87d940a7765c1d01a8a676cb8918b717
MrLikviduk <v@086.kz>
2018-07-08 10:09:40 +0600

    test

--- elements/media-template.php
+++ elements/media-template.php
@@ -18,10 +18,10 @@
             <div class="date"><?=htmlspecialchars($date, ENT_QUOTES, 'UTF-8')?></div>
             <div class="answer" id="answer<?=$id?>"><?=htmlspecialchars($answer, ENT_QUOTES, 'UTF-8')?></div>
             <?php if (can_do('answer_questions_for_media')) { ?>
-                <form action="javascript:void(null);" method="POST" id="answer_form<?=$id?>" onsubmit="call_answer(<?=$id?>)">
+                <form action="javascript:void(null);" method="POST" id="answer_form<?=$id?>" onsubmit="call_answer(<?=$id?>)" style="margin: 10px 0;">
                     <label for="answer"><?=translate('Ответить на вопрос')?>: </label>
                     <br>
-                    <textarea name="answer" id="answer_id<?=$id?>" rows="10" style="width: 100%;" maxlength="300"></textarea>
+                    <textarea name="answer" id="answer_id<?=$id?>" rows="6" style="width: 100%;" maxlength="300"></textarea>
                     <br>
                     <input type="hidden" name="qa_id" value=<?=$id?>>
                     <input type="submit" value="<?=translate('Ответить')?>" style="padding: 10px;" name="submit">
