5b1dc10642abea8e8301a4ce321ba7f6158a012f
MrLikviduk <v@086.kz>
2018-07-09 15:17:20 +0600

    test

--- header.php
+++ header.php
@@ -17,6 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <meta name="description" content="We are a new company specializing in the development and reissue of mobile games. Our goal is to bring people the most driving, unique and memorable game experience.">
     <link rel="stylesheet" href="/styles/style.css?v<?=time()?>">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="/js_scripts/jquery.cookie.js"></script>
