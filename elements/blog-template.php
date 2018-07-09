15856b1d873f5ef2a5c725b7b346c02a856a5cb6
MrLikviduk <v@086.kz>
2018-07-09 21:24:00 +0600

    test

--- elements/blog-template.php
+++ elements/blog-template.php
@@ -68,7 +68,7 @@
             echo '</div>';
             if (($for_media ? $_SESSION['id_to_edit_media_comment'] : $_SESSION['id_to_edit_comment']) == $id)
                 echo "<script>
-                    document.getElementById('comment_content".get_by_id(($for_media ? $_SESSION['id_to_edit_media_comment'] : $_SESSION['id_to_edit_comment']), 'comments')['blog_id']."').innerHTML = '".htmlspecialchars(get_by_id($_SESSION['id_to_edit_media_comment'], 'comments')['content'], ENT_QUOTES, 'UTF-8')."';
+                    document.getElementById('comment_content".get_by_id(($for_media ? $_SESSION['id_to_edit_media_comment'] : $_SESSION['id_to_edit_comment']), 'comments')['blog_id']."').innerHTML = '".htmlspecialchars(get_by_id(($for_media ? $_SESSION['id_to_edit_media_comment'] : $_SESSION['id_to_edit_comment']), 'comments')['content'], ENT_QUOTES, 'UTF-8')."';
                 </script>";
     }
 ?>
