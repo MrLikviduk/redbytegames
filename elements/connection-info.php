b9454c894f901d0638f171c2ead9ae3ba016282f
MrLikviduk <v@086.kz>
2018-07-10 13:37:37 +0600

    v1.0

--- elements/connection-info.php
+++ elements/connection-info.php
@@ -2,7 +2,7 @@
     $host_name = 'srv-pleskdb16.ps.kz:3306';
     $db_name = 'redbyteg_users';
     $db_username = 'redby_proger';
-    $db_password = 'imgnida1234';
+    $db_password = 'JzvCeTiaM8E@6KAD';
     // $host_name = 'localhost';
     // $db_name = 'users';
     // $db_username = 'root';
