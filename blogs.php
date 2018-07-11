f791b04f7af456c7f9ae23d77b18f5fb6b9ffad7
MrLikviduk <v@086.kz>
2018-07-11 13:05:01 +0600

    test

--- blogs.php
+++ blogs.php
@@ -62,7 +62,7 @@
             $comment = get_by_id($_POST['block_form_user_id'], 'comments');
             $blog_id = $comment['blog_id'];
             if ($_POST['block_form_days'] >= 0 && $_POST['block_form_hours'] >= 0)
-                $ban_time = (int)$_POST['block_form_days'] * 86400 + (int)$_POST['block_form_hours'] * 3600;
+                $ban_time = $_POST['block_form_days'] * 86400 + $_POST['block_form_hours'] * 3600;
             else
                 $ban_time = 0;
             if (is_legal($_POST['block_form_comment'], 0, 300)) {
