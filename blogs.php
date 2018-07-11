b6109d90f921a433bba49c43b435673f715b1e88
MrLikviduk <v@086.kz>
2018-07-11 14:00:36 +0600

    test

--- blogs.php
+++ blogs.php
@@ -71,7 +71,7 @@
                 $ban_comment = $_POST['block_form_comment'];
             else
                 $ban_comment = '';
-            $user_id = (int)$_POST['block_form_user_id'];
+            $user_id = get_by_id($_POST['block_form_user_id'], 'comments')['user_id'];
             $mysqli->query("DELETE FROM comments WHERE `user_id` = $user_id") or die("ERROR111");
             set_data('users', 'id', $user_id, 'banned', 1) or die("ERROR1");
             set_data('users', 'id', $user_id, 'unban_time', intval(date('U')) + $ban_time) or die("ERROR1");
