6e3bde9d648786607ca4267fd026430b2de9bf5d
MrLikviduk <v@086.kz>
2018-07-11 17:29:16 +0600

    test

--- elements/blog-result.php
+++ elements/blog-result.php
@@ -6,8 +6,6 @@
     if (isset($_POST['comment_content']) && is_legal($_POST['comment_content'], 1, 1023) && can_do('add_comments') && get_field($_SESSION['login'], 'banned') == '0') {
         $user_id = get_id_by_username($_SESSION['login']);
         $blog_id = $_POST['blog_id'];
-        $result = $mysqli->query("SELECT * FROM comments WHERE user_id = $user_id and blog_id = $blog_id ORDER BY id DESC");
-        $comment = $result->fetch_assoc();
         if (isset($_SESSION['id_to_edit_comment']) && get_by_id($_SESSION['id_to_edit_comment'], 'comments')['blog_id'] == $_POST['blog_id'] && user_is_set($_SESSION['login'], $_SESSION['password']) && get_id_by_username($_SESSION['login']) == get_by_id($_SESSION['id_to_edit_comment'], 'comments')['user_id']) {
             $comment_content = $mysqli->real_escape_string($_POST['comment_content']);
             $id_to_edit_comment = (int) $_SESSION['id_to_edit_comment'];
@@ -17,8 +15,10 @@
         }
         else {
             add_comment($blog_id, $user_id, date('d.m.Y'), date('H:i'), $_POST['comment_content']);
-            $id = (int)$comment['id'];
         }
+        $result = $mysqli->query("SELECT * FROM comments WHERE user_id = $user_id and blog_id = $blog_id ORDER BY id DESC");
+        $comment = $result->fetch_assoc();
+        if (!isset($id)) $id = (int)$comment['id'];
     }
     show_comment(get_by_id($comment['user_id'], 'users')['username'], $comment['creation_date'], $comment['creation_time'], $comment['content'], $id);
 ?>
