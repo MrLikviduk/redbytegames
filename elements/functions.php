0165edad73bf855603b62c39303ea009d222c598
MrLikviduk <v@086.kz>
2018-07-13 05:52:27 +0600

    test

--- elements/functions.php
+++ elements/functions.php
@@ -300,8 +300,6 @@
                     return 'Moderator\'s comment';
                 case 'Поздравляем! Вы успешно вошли под своей учетной записью!':
                     return 'Congratulation! You have successfully logged in to your account!';
-                case 'Введите комментарий':
-                    return 'Enter a comment';
             }
         if ($_SESSION['lang'] == 'ru')
             return $s;
