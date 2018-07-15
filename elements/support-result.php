9e329d69a53058ac16c9e51d27562aeacf4766a6
MrLikviduk <v@086.kz>
2018-07-15 08:05:55 +0600

    test

--- elements/support-result.php
+++ elements/support-result.php
@@ -8,8 +8,10 @@
         $message = $mysqli->real_escape_string($_POST['message']);
         mail('support@redbytegames.ru', 'Новый вопрос', 'ФИО: '.$_POST['fullname']."\r\nПочта: ".$_POST['email']."\r\nСообщение: ".$_POST['message'], "From: support@redbytegames.ru");
         $mysqli->query("INSERT INTO support_requests (id, fullname, `email`, `message`) VALUES (NULL, '$fullname', '$email', '$message')") or die("database error");
-        die('success');
+        echo 'success';
+        exit();
     }
-    die(translate('incorrect data'));
+    echo 'incorrect data';
+    exit();
     $mysqli->close();
 ?>
