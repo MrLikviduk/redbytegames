6d34507b9edc6b513080a7f9e47cd635cfa29b63
MrLikviduk <v@086.kz>
2018-07-18 13:41:33 +0600

    Add social networks

--- footer.php
+++ footer.php
@@ -3,17 +3,17 @@
         © RedByteGames 2018
         <div class="socnet-wrapper">
             <?=translate('Следите за нами')?>: 
-            <a href="https://vk.com" target="_blank">
+            <a href="https://vk.com/rbg_<?=(isset($_SESSION['lang']) && $_SESSION['lang'] == 'ru' ? 'ru' : 'eng')?>" target="_blank">
                 <img src="/img/soc_icons/vkontakte.png" alt="VK" class="socnet">
             </a>
             <a href="https://facebook.com" target="_blank">
                 <img src="/img/soc_icons/facebook.png" alt="FB" class="socnet">
             </a>
-            <a href="https://twitter.com" target="_blank">
+            <a href="https://twitter.com/games_byte" target="_blank">
                 <img src="/img/soc_icons/twitter.png" alt="Twitter" class="socnet">
             </a>
-            <a href="https://youtube.com" target="_blank">
-                <img src="/img/soc_icons/youtube.png" alt="YouTube" class="socnet">
+            <a href="https://www.instagram.com/redbytegames/" target="_blank">
+                <img src="/img/soc_icons/instagram.png" alt="Instagram" class="socnet">
             </a>
         </div>
     </footer>
